5d99b3e7d7a5cf8db38386a911d64dd9089c0e9d
msg768 <markus.ghambari@gmail.com>
2020-04-23 13:34:10 +1000

    swiftcash

--- src/networks.js
+++ src/networks.js
@@ -33,3 +33,14 @@ exports.testnet = {
   scriptHash: 0xc4,
   wif: 0xef,
 };
+exports.swiftcash = {
+  messagePrefix: '\x18SwiftCash Signed Message:\n',
+  bip32: {
+    public: 0x0488b21e,
+    private: 0x0488ade4
+  },
+  pubKeyHash: 0x3f,
+  scriptHash: 0x12,
+  wif: 0x98
+};
+
